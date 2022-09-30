b9707cb1ff766916ad5b74ffd8dbb5182b434cd1
andsel <selva.andre@gmail.com>
2022-09-30 13:31:48 +0200

    Moved docker images and csv in the locations expected by the release manager

--- ci/dra_upload.sh
+++ ci/dra_upload.sh
@@ -68,6 +68,16 @@ gsutil cp gs://logstash-ci-artifacts/dra/${STACK_VERSION}/dependencies-reports/l
 echo "Downloaded ARTIFACTS"
 for file in build/logstash-*; do shasum $file;done
 
+# reposition files as expected by the release-manager
+mv build/logstash-${STACK_VERSION}-docker-image-x86_64.tar.gz .
+mv build/logstash-oss-${STACK_VERSION}-docker-image-x86_64.tar.gz .
+mv build/logstash-ubi8-${STACK_VERSION}-docker-image-x86_64.tar.gz .
+mv build/logstash-${STACK_VERSION}-docker-image-aarch64.tar.gz .
+mv build/logstash-oss-${STACK_VERSION}-docker-image-aarch64.tar.gz .
+mv build/logstash-ubi8-${STACK_VERSION}-docker-image-aarch64.tar.gz .
+
+mkdir -p build/distributions/dependencies-reports/
+mv build/logstash-${STACK_VERSION}.csv build/distributions/dependencies-reports/
 
 # # set required permissions on artifacts and directory
 # chmod -R a+r build/*
